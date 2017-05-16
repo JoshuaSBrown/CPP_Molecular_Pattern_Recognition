a076717f981b9f4eb0e28b9906a9180322e9ac5f
Carlo <carlo@localhost.localdomain>
2017-05-16 01:06:50 -0600

    Counting bonds

--- Readfile.cpp
+++ Readfile.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <stdio.h>
 #include <vector>
@@ -12,6 +13,7 @@ int main() {
   ifstream File;
   File.open("Examples/carbonmolecule2.pdb");
   string line;
+  std::vector<Atom> atmV;
 
   if(File.is_open())
   {
@@ -20,6 +22,20 @@ int main() {
     {
       getline(File,line);
       lineNo++;
+      std::vector<int> bonds;
+
+      if (lineNo > 14 && lineNo < 26)
+      {
+        std::istringstream iss(line);
+        for(std::string line2; iss >> line2;){
+          if(line2 != "CONECT"){
+            bonds.push_back(stoi(line2));
+          }
+        }
+        atmV.at(bonds.at(0)).bonds = bonds.size()-1;
+        atmV.at(bonds.at(0)).print();
+        }
+
       if (lineNo > 2 && lineNo < 15)
       {
         char str[100];
@@ -32,10 +48,9 @@ int main() {
         int i;
         sscanf(line.c_str(),"%s %d %s %s %d %lf %lf %lf",str, &i, atm, str1, &j, &tempX, &tempY, &tempZ);
         Atom a = Atom(atm, i, tempX, tempY, tempZ);
-        a.print();
+        atmV.push_back(a);
       }
     }
-
   }
 
   File.close();
