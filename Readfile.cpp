b5365a995a2729b15c5dbe14b09ce86cae6e8405
Carlo <carlo@localhost.localdomain>
2017-05-24 05:37:34 -0600

    working on a better way to read the file, and adding neighboring atoms to the struct

--- Readfile.cpp
+++ Readfile.cpp
@@ -17,30 +17,30 @@ int main() {
 
   if(File.is_open())
   {
-    int lineNo = 0;
+
     while(!File.eof())
     {
       getline(File,line);
-      //cout << line << endl;
-      //cout << line[0] << endl;
-      lineNo++;
-      vector<int> bonds;
-      vector<string> neighbors;
-      if (lineNo > 14 && lineNo < 27)
-      //if ((line[0] == 'C') and (line[5] == 'T'))  //I was trying this, since the format (i.e CONECT , etc) should be
-                                                    //the same for each pdf file, but the number of lines is not
+      //cout << line << endl; //prints the entire pdb file
+      //cout << line[0] << endl; //prints the first char of each line
+
+      vector<int> bonds;  //stores the IDs of the connected atoms
+      vector<string> neighbors; //stores the atom types
+
+      if ((line[0] == 'C') and (line[5] == 'T'))
       {
-        istringstream iss(line);                    // do we need BOTH getline and istringstream?
-        for(string line2; iss >> line2;){
-          //cout << line2 << endl;
-          if(line2 != "CONECT"){
-            bonds.push_back(stoi(line2));
-            //cout << stoi(line2) << endl;
+        istringstream iss(line);
+        for(string line2; iss >> line2;)
+        {
+          //cout << line2 << endl;  //prints "CONECT" and connections
+          if(line2 != "CONECT")
+          {
+            bonds.push_back(stoi(line2)); //inserts IDs of connections into the bonds vector
           }
         }
 
         for (unsigned int i=0; i<bonds.size(); i++){
-          //cout << bonds[i] << endl;
+          //cout << bonds[i] << endl; //prints content of bonds vector: IDs of atom and its connections
           for (unsigned int j=0; j<atmV.size(); j++){
             if (atmV.at(j).id == bonds[i]){
               //cout << atmV.at(j).atom_type << endl;
@@ -48,15 +48,16 @@ int main() {
             }
           }
         }
+
         for (unsigned int i=1; i<neighbors.size();i++){
-          cout << neighbors[i] << endl;
+          cout << neighbors[i] << endl; //prints the bonded atoms
+          //atmV.neighbors.push_back(neighbors[i]);   //??
         }
         atmV.at(bonds.at(0)).bonds = bonds.size()-1;
         atmV.at(bonds.at(0)).print();
-        }
+      }
 
-      //if (line[0] == 'H'){
-      if (lineNo > 1 && lineNo < 15)
+      else if (line[0] == 'H')
       {
         char str[100];
         char str1[100];
@@ -67,8 +68,8 @@ int main() {
         double tempZ;
         int i;
         sscanf(line.c_str(),"%s %d %s %s %d %lf %lf %lf",str, &i, atm, str1, &j, &tempX, &tempY, &tempZ);
-        Atom a = Atom(atm, i, tempX, tempY, tempZ);
-        atmV.push_back(a);
+        Atom a = Atom(atm, i, tempX, tempY, tempZ);  //builds an atom with its type, ID and position.
+        atmV.push_back(a);                       //inserts the atom in the vecor of atom structs
       }
     }
   }
